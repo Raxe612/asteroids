14c1e3cd32dcc877881cec258474c2da5dae9563
Raxe612 <martin.ramin612@gmail.com>
2026-04-23 10:40:56 +0200

    added delta and game time

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    
     while True:
         log_state()
         for event in pygame.event.get():
@@ -15,6 +18,8 @@ def main():
                 return
         screen.fill("black")
         pygame.display.flip()
+        dt = clock.tick(60)
+        print(dt)
 
 
 
